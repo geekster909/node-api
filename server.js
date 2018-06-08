3f96da5606aea5554605dbb4c24932e79d847885
Justin Bond <bond.justink@gmail.com>
2018-06-08 10:04:11 -0700

    setup parameters, created post endpoint

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
-
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
+const database             = require('./config/db');
 
 const app            = express();
 
 const port = 8000;
 
-require('./app/routes')(app, {});
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, () => {
-  console.log('We are live on ' + port);
-});
+MongoClient.connect(database.url, (err, database) => {
+	if (err) return console.log(err)
+
+	db = database.db("node_api_notes")
+	require('./app/routes')(app, db);
+
+	app.listen(port, () => {
+		console.log('We are live on ' + port);
+	});
+})
